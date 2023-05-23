045ac9c2641ffcc241b8bdbd0eebeaa093ed4c5b
Victor Trimpai <victor.trimpai1987@gmail.com>
2023-05-23 18:08:40 -0400

    Add files via upload

---
+++ C#/Calculadora/Calculadora/Metodos.cs
@@ -0,0 +1,91 @@
+﻿using System;
+namespace Calculadora
+{
+	public class Metodos
+	{
+
+        private int suma;
+        private int resta;
+        private float multi;
+        private double divi;
+        private int num1;
+        private int num2;
+
+        public Metodos()
+		{
+		}
+
+
+
+
+        public int getNum1()
+        {
+            return num1;
+        }
+
+        public void setNum1(int num1)
+        {
+            this.num1 = num1;
+        }
+
+        public int getNum2()
+        {
+            return num2;
+        }
+
+        public void setNum2(int num2)
+        {
+            this.num2 = num2;
+        }
+
+        public int getSuma()
+        {
+            int suma = num1 + num2;
+            return suma;
+        }
+
+        public void setSuma(int suma)
+        {
+            this.suma = suma;
+        }
+
+        public int getResta()
+        {
+            int resta = num1 - num2;
+            return resta;
+        }
+
+        public void setResta(int resta)
+        {
+            this.resta = resta;
+        }
+
+        public float getMulti()
+        {
+            float multi = num1 * num2;
+            return multi;
+        }
+
+        public void setMulti(float multi)
+        {
+            this.multi = multi;
+        }
+
+        public double getDivi()
+        {
+            double divi = num1 / num2;
+            return divi;
+        }
+
+        public void setDivi(double divi)
+        {
+            this.divi = divi;
+        }
+
+
+
+
+
+    }
+}
+
---
+++ C#/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Calculadora // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la Calculadora");
+
+            Metodos m = new Metodos();
+
+            Console.WriteLine("Ingrese Numero 1");
+            m.setNum1(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Ingrese Numero 2");
+            m.setNum2(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Seleccione operacion: ");
+            Console.WriteLine("");
+            Console.WriteLine("1.- Suma");
+            Console.WriteLine("2.- Resta");
+            Console.WriteLine("3.- Multiplicacion");
+            Console.WriteLine("4.- Division");
+            Console.WriteLine("5- Potenciación.");
+            
+
+            int caso = Convert.ToInt32(Console.ReadLine());
+            switch (caso) {
+                case 1:
+                   Console.WriteLine("La suma de los numeros enteros es: " + m.getSuma());
+                    break;
+                case 2:
+                    Console.WriteLine("La resta de los numeros entero es: " + m.getResta()  );
+                    break;
+                case 3:
+                    Console.WriteLine("La multiplicacion de los numeros entero es: " + m.getMulti());
+                    break;
+                case 4:
+                    Console.WriteLine("La division de los numeros entero es: " + m.getDivi());
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion no valida.");
+                    break;
+            }
+        }
+    }
+}
