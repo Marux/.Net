5a8d95f6fcd020a099e35b1c1448698d39dd0c93
Victor Trimpai <victor.trimpai1987@gmail.com>
2023-05-23 18:09:30 -0400

    Add files via upload

---
+++ C#/Ejercicio3/Ejercicio3/Caja.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Ejercicio3
+{
+	public class Caja
+	{
+        private int saldo;
+
+        public int getSaldo()
+        {
+            return saldo;
+        }
+
+        public void setSaldo(int saldo)
+        {
+            this.saldo = saldo;
+        }
+
+        public void Depositar(int monto)
+        {
+            this.saldo = this.saldo + monto;
+        }
+
+
+    }
+}
+
---
+++ C#/Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+namespace Ejercicio3 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente c = new Cliente();
+            Contador con = new Contador();
+            Caja ca = new Caja();
+            Cobrador cob = new Cobrador();
+
+            ca.setSaldo(20000);
+            c.setNombre("Victor Trimpai");
+            con.setCajita(ca);
+            Console.WriteLine("Bienvenido al Sistema de Prestamo: " + c.getNombre());
+
+            Console.WriteLine("El saldo de la caja es: " + ca.getSaldo());
+            Console.WriteLine("Ingrese monto a solicitar: ");
+            int montoprestamo = Int32.Parse(Console.ReadLine());
+            int deudatotalcliente = con.Prestar(montoprestamo);
+
+            c.setMontocredito(montoprestamo);
+            c.setDeuda(deudatotalcliente);
+
+            Console.WriteLine(c.getNombre() + " Usted pidio la suma de: " + c.getMontocredito());
+            Console.WriteLine(c.getNombre() + " Y su deuda total es: " + c.getDeuda());
+
+
+            Console.WriteLine(c.getNombre() + " Favor ingrese el mondo a devolver: ");
+            int montodevuelto = Int32.Parse(Console.ReadLine());
+
+            cob.Cobrar(montodevuelto);
+
+            int recaudadototal = cob.Devolver();
+            con.Devolver(recaudadototal);
+
+            Console.WriteLine("El saldo de la caja es de: " + ca.getSaldo());
+
+
+
+
+        }
+    }
+}
